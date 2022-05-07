4ee75d3a7a0d571c4e933fb0d07c417732e3e8d9
Steerpike <imthekoul@gmail.com>
2022-05-08 01:23:27 +0300

    API Request - resolved

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('yeah');
+const img = document.querySelector('img');
+fetch(
+  'https://api.giphy.com/v1/gifs/translate?api_key=TucjQQqfWk53eCLWY4cidBL0mlGL8f6s&s=cats',
+  {
+    mode: 'cors',
+  }
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(response => {
+    img.src = response.data.images.original.url;
+  });
