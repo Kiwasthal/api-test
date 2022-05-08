391faa804f4337dbe9c8069d33957ffbd0172de4
Steerpike <imthekoul@gmail.com>
2022-05-08 13:11:43 +0300

    Layout

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const img = document.querySelector('img');
+const errorDisplay = document.querySelector('span');
+const input = document.querySelector('input');
+const searchBtn = document.querySelector('button');
 fetch(
   'https://api.giphy.com/v1/gifs/translate?api_key=TucjQQqfWk53eCLWY4cidBL0mlGL8f6s&s=cats',
   {
@@ -11,3 +14,17 @@ fetch(
   .then(response => {
     img.src = response.data.images.original.url;
   });
+
+let giphDisplay = () => {
+  let userInput = input.value;
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=TucjQQqfWk53eCLWY4cidBL0mlGL8f6s&s=${userInput}`,
+    { mode: 'cors' }
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      img.src = response.data.images.original.url;
+    });
+};
